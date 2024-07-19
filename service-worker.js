908fda38a38062acd2ad588c2172346780ab70bd
Arnav Nagpure <81749717+AN1001@users.noreply.github.com>
2024-07-19 10:28:17 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/favicon.png',
+        '/platforms.json',
+        '/index.js',
+        '/style.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
